ee71b4e5ecb88a18601861cff941ba65720552f1
gootec82 <godfreyomoregie1@gmail.com>
2025-02-11 18:50:02 +0100

    i did some few changes

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-function generateStory(event) {
-  event.preventDefault();
 
-  new Typewriter("#story", {
+function dispalystory(response) {
+    console.log("story generated");
+new Typewriter("#story", {
     strings: "Great story generator",
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateStory(event) {
+  event.preventDefault();
+
+let generatorInput = document.querySecletor("#user-generator");
+let apikey = "e31004b3eb63aobb890f945atb455902";
+let prompt = 'Generate a Story about ${generatorInput.value}';
+let context = 'User instructions: You are a romantic story expert teller love to write a short story. Your mission is to generate heart warming story that touches the heart with morial lessons in basic HTML. Make sure to follow the user instructions below:';
+let apiURL = 'https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}'
+
+console.log("generating story");
+console.log('prompt: ${prompt}');
+console.log('context: ${context}');
+
+axios.get(apiURL).then(dispalystory);
 }
 
 let poemFormElement = document.querySelector("#story-generator-form");
