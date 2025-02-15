77d102955170220ca29e8b4b83833676e187c34a
gootec82 <godfreyomoregie1@gmail.com>
2025-02-15 21:01:15 +0100

    i did major changes to the HTML , CSS and JS

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-
-function dispalystory(response) {
-    console.log("story generated");
-new Typewriter("#story", {
-    strings: "Great story generator",
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
 
-function generateStory(event) {
+function generatePoem(event) {
   event.preventDefault();
 
-let generatorInput = document.querySecletor("#user-generator");
-let apikey = "e31004b3eb63aobb890f945atb455902";
-let prompt = 'Generate a Story about ${generatorInput.value}';
-let context = 'User instructions: You are a romantic story expert teller love to write a short story. Your mission is to generate heart warming story that touches the heart with morial lessons in basic HTML. Make sure to follow the user instructions below:';
-let apiURL = 'https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}'
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "e31004b3eb63aobb890f945atb455902";
+  let context =
+    "You are an intelligent AI please give accurate answer to any questions that is been ask by anybody. please make sure the answer is correct and straight forard to the point . You mission is to generate any question in basic HTML and separate each line with a <br /> dont mention basic html. Make sure to follow the user instructions. Sign with 'DevSpark AI' inside a <strong> element at the end of your conclution and NOT at the beginning. please give current answer.";
+  let prompt = `User instructions: Give an answer to any question about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-console.log("generating story");
-console.log('prompt: ${prompt}');
-console.log('context: ${context}');
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating your request ${instructionsInput.value}</div>`;
 
-axios.get(apiURL).then(dispalystory);
+  axios.get(apiURL).then(displayPoem);
 }
 
-let poemFormElement = document.querySelector("#story-generator-form");
-poemFormElement.addEventListener("submit", generateStory);
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
